23318cdb500446973dc16b866ab7b3bedc147e99
jathek <692914+jathek@users.noreply.github.com>
2025-01-30 00:00:08 -0600

    feat(unraid-webgui-mods): update script for unbalanced

--- userscripts/unraid-webgui-mods.user.js
+++ userscripts/unraid-webgui-mods.user.js
@@ -1,21 +1,22 @@
 // ==UserScript==
 // @name         unRAID - WebGUI Modifications
 // @namespace    https://github.com/jathek/
-// @version      2023.4.15
-// @description  unRAID
 // @author       jathek
+// @version      2023.4.15
+// @updateURL    https://github.com/jathek/firefox-tweaks/raw/main/userscripts/unraid-webgui-mods.user.js
+// @downloadURL  https://github.com/jathek/firefox-tweaks/raw/main/userscripts/unraid-webgui-mods.user.js
+// @description  customize unraid web GUI
 // @icon         https://icons.duckduckgo.com/ip2/unraid.net.ico
 // @grant        none
 // ==/UserScript==
 
+"use strict";
 (function () {
-  "use strict";
-  // Your code here...
+  // remove copyright info from statusbar
   document.querySelector("span#copyright").innerText = "";
-  // replace unbalance link with traefik link
-  if (document.location.toString().includes("/Settings/unbalance")) {
-    let unbalanceLink = document.querySelector('a[title="unBALANCE plugin"]');
-    let unraidHost = document.location.hostname.toString().replace("unraid.", "");
-    unbalanceLink.href = `https://unbalance.${unraidHost}`;
+  // replace unbalanced link with subdomain link
+  if (document.location.pathname.includes("/Settings/unbalanced")) {
+    const unbalancedLink = document.querySelector('a[title="unbalanced plugin"]');
+    unbalancedLink.href = `https://unbalanced.${document.location.hostname.replace(/(?:.*?\.)?(.*?\..*?)$/, "$1")}`;
   }
 })();
